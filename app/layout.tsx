f45f03b0ec70ac92fddcec4dd45a7a8b52e9da25
Lixdevv <jorgeleython2@gmail.com>
2025-11-08 17:02:40 -0500

    Change title from 'Pepe Agent' to 'mascalo'

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Pepe Agent",
+  title: "mascalo",
   description: "Created with PF dev",
   icons: {
     icon: "https://ibb.co/m58Srzpp",
