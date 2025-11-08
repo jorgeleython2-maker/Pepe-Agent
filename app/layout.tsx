565cb15c886c8c1d0a1b91f27c39e1e35c3bd2f2
Lixdevv <jorgeleython2@gmail.com>
2025-11-08 03:43:20 -0500

    Commit inicial: Subiendo mi carpeta

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Pepe Agent",
   description: "Created with PF dev",
-  generator: "",
+  generator: "https://ibb.co/Xx6rrS2f",
 }
 
 export default function RootLayout({
